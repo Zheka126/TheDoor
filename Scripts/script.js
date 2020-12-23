4256f48aa600380990067e0ff4f5605bac69ee53
Женя <prototype-zheka@list.ru>
2020-12-23 18:44:21 +0200

    Updated

--- Scripts/script.js
+++ Scripts/script.js
@@ -1,33 +1,52 @@
+//аудио
 var scream = document.querySelector('.scream');
-var stook = document.querySelector('.stook');
+var stook1 = document.querySelector('.stook1');
+var stook2 = document.querySelector('.stook2');
+var stook3 = document.querySelector('.stook3');
 var tresk = document.querySelector('.tresk');
-var cracks = document.querySelector('.cracks');
+
+//объекты страницы
 var door = document.querySelector('.door');
 var screamer = document.querySelector('.screamer');
-var header = document.querySelector('.header');
+var cracks = document.querySelector('.cracks');
 var footer = document.querySelector('.footer')
-console.log(cracks);
 var clicks = 0;
 
 door.addEventListener("click", function () {
-    clicks = clicks + 1;
+    clicks++;
     console.log(clicks);
-    if (clicks == 1) {
-        stook.play();
-    } else if (clicks == 2) {
-        door.classList.add('tremor');
-        cracks.classList.remove('visually-hidden');
-        tresk.play();
-    } else if (clicks == 3) {
-        scream.play();
-        screamer.classList.remove('visually-hidden');
-        door.classList.remove('tremor');
-        cracks.classList.add('visually-hidden');
-        screamer.classList.add('tremor');
-        setTimeout(function () {
-            screamer.classList.add('visually-hidden');
-        }, 2500)
-        clicks = 0;
+
+    switch (clicks) {
+        case 1:
+            stook1.play();
+            break;
+
+        case 2:
+            stook2.play();
+            break;
+
+        case 3:
+            stook3.play();
+            break;
+
+        case 4:
+            door.classList.add('tremor');
+            cracks.classList.remove('visually-hidden');
+            tresk.play();
+            break;
+
+        case 5:
+            scream.play();
+            screamer.classList.remove('visually-hidden');
+            door.classList.remove('tremor');
+            cracks.classList.add('visually-hidden');
+            screamer.classList.add('tremor');
+
+            setTimeout(function () {
+                screamer.classList.add('visually-hidden');
+            }, 2500)
+            clicks = 0; //Внутри последнего случая обязательно нужно обнулять значение этого счётчика
+            break;
     };
 
 });
